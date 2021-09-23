efd20f82b08a19cd34214a0860545f9f34335bc2
Eduardo Guerra Sabino <sabino.eduardo.guerra@gmail.com>
2021-09-23 19:05:36 +0100

    chore: get user input
    even though our form was being displayed to
    the user, it was not getting their input, so we decided to solve this problem

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,29 +1,54 @@
+import { useRef } from 'react'
+
 import classes from "./NewMeetupForm.module.css";
 import Card from "../ui/Card";
 
 const NewMeetupForm = () => {
+  const titleInputRef = useRef()
+  const imageInputRef = useRef() 
+  const addressInputRef = useRef()
+  const descriptionInputRef = useRef()
+
+  function submitHandler(event) {
+    event.preventDefault()
+
+    const enteredTitle = titleInputRef.current.value
+    const enteredImage = imageInputRef.current.value
+    const enteredAddress = addressInputRef.current.value
+    const enteredDescription = descriptionInputRef.current.value
+
+    const item = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    }
+    
+    console.log(item)
+  }
+
   return (
     <div>
       <Card>
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandler}>
           <div className={classes.control}>
             <label htmlFor="title">Title</label>
-            <input type="text" required id="title" />
+            <input type="text" required id="title" ref={titleInputRef} />
           </div>
           <div className={classes.control}>
             <label htmlFor="image">Image</label>
-            <input type="url" required id="image" />
+            <input type="url" required id="image" ref={imageInputRef} />
           </div>
           <div className={classes.control}>
             <label htmlFor="address">Address</label>
-            <input type="text" required id="address" />
+            <input type="text" required id="address" ref={addressInputRef} />
           </div>
           <div className={classes.control}>
             <label htmlFor="description">Description</label>
-            <textarea required id="description" rows='5' ></textarea>
+            <textarea required id="description" rows='5' ref={descriptionInputRef} ></textarea>
           </div>
           <div className={classes.actions}>
-              <button>Add</button>
+            <button>Add</button>
           </div>
         </form>
       </Card>
