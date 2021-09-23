79c4016848c59f61356ab581758d6ffd58ecd0e9
Eduardo Guerra Sabino <sabino.eduardo.guerra@gmail.com>
2021-09-23 21:58:36 +0100

    feat: display all meetups
    with this change we get all meetups from firebase, then display them on the screen

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,33 +1,47 @@
-import MeetupList from "../components/meetups/MeetupList"
-
-const items = [
-    {
-        id: 'm1',
-        title: 'This is a first meetup',
-        image:
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Meetupstreet 5, 12345 Meetup City',
-        description:
-            'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-    {
-        id: 'm2',
-        title: 'This is a second meetup',
-        image:
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Meetupstreet 5, 12345 Meetup City',
-        description:
-            'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-]
+import { useState, useEffect } from "react";
+import MeetupList from "../components/meetups/MeetupList";
 
 const AllMeetups = () => {
-    return (
-        <section>
-            <h1>All Meetups</h1>
-            <MeetupList items={items}/>
-        </section>
-    )
-}
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+  
+  function dataHandler(data) {
+    const meetups = [];
+    for (const key in data) {
+      const meetup = {
+        id: key,
+        ...data[key],
+      };
+      meetups.push(meetup);
+    }
 
-export default AllMeetups
+    return meetups;
+  }
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://react-course-6115f-default-rtdb.firebaseio.com/meetup.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const meetups = dataHandler(data);
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList items={loadedMeetups} />
+    </section>
+  );
+};
+
+export default AllMeetups;
