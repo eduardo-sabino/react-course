121960d34bcc63809ee44cfcdd40477e4e93d15f
Eduardo Guerra Sabino <sabino.eduardo.guerra@gmail.com>
2021-09-24 21:03:40 +0100

    refactor: make a responsive navigation bar

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,25 +1,31 @@
-import { Link } from "react-router-dom"
-import classes from './MainNavigation.module.css'
+import { Link } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
-    return (
-        <header className={classes.header}>
-            <div className={classes.logo}>React Meetups</div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to='/'>All Meetups</Link>
-                    </li>
-                    <li>
-                        <Link to='/new-meetup'>New Meetup</Link>
-                    </li>
-                    <li>
-                        <Link to='/favorites'>My Favorites</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
+  return (
+    <header className={classes.header}>
+      <label className={classes.icon} htmlFor="iconId">
+        <i className="fa fa-bars"></i>
+      </label>
+      <div className={classes.logo}>
+        React <span>Meetups</span>
+      </div>
+      <nav>
+        <input type="checkbox" id="iconId" />
+        <ul>
+          <li>
+            <Link to="/">All Meetups</Link>
+          </li>
+          <li>
+            <Link to="/new-meetup">New Meetup</Link>
+          </li>
+          <li>
+            <Link to="/favorites">My Favorites</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default MainNavigation
+export default MainNavigation;
